0a053a8226189aee206f7ef0bfefb7757468a31a
Limeade <116500605+LimeaidDev@users.noreply.github.com>
2024-03-06 09:06:57 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -5,11 +5,9 @@ rem Check if the Python environment folder exists
 if not exist %env_folder% (
     echo Creating Python environment...
     
-    rem Create the Python environment (replace this with your actual command)
+    rem Create the Python environment
     python -m venv %env_folder%
-)
 
-rem Activate the Python environment (replace this with your activation command)
 call %env_folder%\Scripts\activate
 pip install discord openai==0.28.1 psutil pillow datetime customtkinter aiohttp
 
